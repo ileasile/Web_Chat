c7cf47cded9df3012584ff1bb9c581932640366c
ileasile <leopardile@yandex.ru>
2017-06-08 01:51:39 +0300

    Added stylesheet

--- web/index.php
+++ web/index.php
@@ -2,8 +2,10 @@
 <html>
 <head>
 <meta charset="utf-8">
+<link href="style/chat-view.css" type="text/css"/>
 <title>Chat Room</title>
 </head>
+
 <body>
 
 <div id="chat"> There will be a chat here... </div>
