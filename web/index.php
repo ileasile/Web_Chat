bffe7acb3bf1ed604705b34a1ff746fe84d16e31
ileasile <leopardile@yandex.ru>
2017-06-08 23:08:23 +0300

    Ignoring log files

--- web/index.php
+++ web/index.php
@@ -2,12 +2,32 @@
 <html>
 <head>
 <meta charset="utf-8">
-<link href="style/chat-view.css" type="text/css"/>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"> </script>
+<link href="style/chat-view.css" type="text/css" rel="stylesheet"/>
 <title>Chat Room</title>
 </head>
 
 <body>
 
-<div id="chat"> There will be a chat here... </div>
+<div id="contents">
+    <div id="chat"> 
+        <div id="chat_messages">
+            <div class="msg">
+                Message 1...
+            </div>
+            <div class="msg">
+                Message 2...
+            </div>
+            <div class="msg">
+                Message 3...
+            </div>
+        </div>
+        <div id="chat_controls">
+            <input type="text" id="message_box" />
+            <input type="button" id="send_message_button" value="Send" />
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
